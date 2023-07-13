ab97c0d0fcccefa0bf0ed6e3be72e9b710948e9f
somaftees <somaftees@gmail.com>
2023-07-13 11:19:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (0);
+	return (1);
 }
