a6581c0a20b6829ce60b84921375a3633d33b1dd
somaftees <somaftees@gmail.com>
2023-07-18 12:37:00 -0700

    x10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *print_alphabets_x10 - write alphabets ten times.
+  * Return: Void
+  */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i=0;
+	while(i < 10)
+	{
+		for (c = 'a'; c<= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	       _putchar('\n');
+		i++;
+	}
+}
