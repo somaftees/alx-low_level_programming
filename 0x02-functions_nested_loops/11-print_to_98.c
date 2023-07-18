23dc5e0c1790b0c8f6867d28f217fe10f4d1993f
somaftees <somaftees@gmail.com>
2023-07-18 16:27:17 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,9 +11,9 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (;n <= 98; n++)
+		for (; n <= 98; n++)
 		{
-			if (n ==98)
+			if (n == 98)
 			{
 				printf("%d", n);
 				printf("\n");
@@ -21,7 +21,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d, ",n);
+				printf("%d, ", n);
 			}
 		}
 	}
