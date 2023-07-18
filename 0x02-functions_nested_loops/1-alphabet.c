a84e0a972d04af4d3c95d8ab2b824ac5ea0f65c4
somaftees <somaftees@gmail.com>
2023-07-18 12:07:08 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_alphabet
+  * Return: Void
+  */
+void print_alphabet(void)
+{
+	char c; 
+		for (c='a'; c<='z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+}
