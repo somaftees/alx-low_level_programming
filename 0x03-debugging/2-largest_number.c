48a061808c926aaac1d358ce932dec31516f2130
somaftees <somaftees@gmail.com>
2023-07-19 07:49:43 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,17 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    else if (b > c)
+    else if (b > a && b > c)
     {
         largest = b;
     }
-    else
+    else if (c > b)
     {
         largest = c;
+    }
+    else
+    {
+	    largest = b;
     }
 
     return (largest);
