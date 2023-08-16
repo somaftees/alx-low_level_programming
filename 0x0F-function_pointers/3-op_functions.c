a8434ed7560c6fc5bbed0a81f30318157a4c4f3b
somaftees <somaftees@gmail.com>
2023-08-16 13:36:30 -0700

    function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,57 +3,48 @@
  * op_add - add function
  * @a: first num
  * @b: second num
- * Return: sum
+ * Return: sum..
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_mod - mod function
- * @a: first num
- * @b: second num
- * Return: sum
+ * op_mod - a mod function
+ * @a: integer.
+ * @b: integer.
+ * Return: mod..
  */
-
 int op_mod(int a, int b)
 {
 	return (a % b);
 }
-
 /**
- * op_div - div function
- * @a: first num
- * @b: second num
- * Return: sum
+ * op_div - a div function
+ * @a: integer.
+ * @b: integer.
+ * Return: div..
  */
-
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
- * op_mul - a function 
+ * op_mul - a mul function
  * @a: integer.
  * @b: integer.
- * Return: multible.
+ * Return: mult..
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_sub - a function that sub 2 nums
+ * op_sub - a sub function
  * @a: integer.
  * @b: integer.
  * Return: sub.
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
