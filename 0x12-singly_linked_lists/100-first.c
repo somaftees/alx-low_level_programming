d6f7954bc1da263f0160d358e20bb65e286b7b38
somaftees <somaftees@gmail.com>
2023-08-24 11:42:53 -0700

    done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * my_fun - a function that prints You're beat! and yet,
+  * you must allow,\nI bore my house upon my back!\n
+  * before the main function is executed.
+  */
+void my_fun(void) __attribute__ ((constructor));
+
+/**
+  * my_fun - implements for my_fun.
+  *
+  * Return: void.
+  */
+void my_fun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
