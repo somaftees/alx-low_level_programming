b5dc000c98d01e511a25b35dadad5773a181a7f9
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-02-10 21:47:14 +0200

    done

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+
+/**
+  * advanced_binary_recursive - advanced_binary_recursive.
+  * @array: array.
+  * @left: left.
+  * @right: right.
+  * @value: value.
+  * Return: -1 or ....
+  */
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (right < left)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
+}
+
+/**
+  * advanced_binary - advanced_binary
+  * @array: array.
+  * @size: size.
+  * @value: value.
+  * Return: NULL, -1.
+  */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return (-1);
+
+	return (advanced_binary_recursive(array, 0, size - 1, value));
+}
